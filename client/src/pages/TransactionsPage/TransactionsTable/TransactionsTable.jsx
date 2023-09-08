27a0cd7b5efb9ac6c21923edc45347aef74809cf
Arteks718 <artexbro.official@gmail.com>
2023-09-08 14:07:21 +0300

    Add data to transaction page

--- client/src/pages/TransactionsPage/TransactionsTable/TransactionsTable.jsx
+++ client/src/pages/TransactionsPage/TransactionsTable/TransactionsTable.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { format } from "date-fns";
 import { getTransactionsThunk } from '../../../store/slices/transactionSlice';
 import Spinner from '../../../components/Spinner/Spinner';
 function TransactionsTable({
@@ -13,37 +14,41 @@ function TransactionsTable({
   }, []);
 
   return (
-    <table>
-      <caption>Transaction Table</caption>
-      <thead>
-        <tr>
-          <th key={1}>Amount</th>
-          <th key={2}>Operation Type</th>
-          <th key={3}>Data</th>
-        </tr>
-      </thead>
-      <tbody>
-        {error ? <p>ERROR</p> : null}
-        {isFetching ? (
-          <p>Test</p>
-        ) : (
-          transactions.map((t) => (
-            <tr key={t.id}>
-              <td key={1}>{t.amount}</td>
-              <td key={2}>{t.operationType}</td>
-              <td key={3}>{new Date(t.createdAt).toUTCString()}</td>
+    <>
+      {error && <div>ERROR!!!</div>}
+      {isFetching && <Spinner />}
+      {!isFetching && !error && !transactions.length && <div>List of transactions is empty</div>}
+      {!isFetching && !error && transactions.length && (
+        <table>
+          <caption>Transaction Table</caption>
+          <thead>
+            <tr>
+              <th key={1}>Amount</th>
+              <th key={2}>Operation Type</th>
+              <th key={3}>Data</th>
             </tr>
-          ))
-        )}
-      </tbody>
-      <tfoot>
-        <tr>
-          <td key={1}>{transactions.reduce((accum, t) => accum + Number(t.amount), 0)}</td>
-          <td key={2}></td>
-          <td key={3}></td>
-        </tr>
-      </tfoot>
-    </table>
+          </thead>
+          <tbody>
+            {transactions.map((t) => (
+              <tr key={t.id}>
+                <td key={1}>{t.amount}</td>
+                <td key={2}>{t.operationType}</td>
+                <td key={3}>{format(new Date(t.createdAt), 'd.MM.yyyy')}</td>
+              </tr>
+            ))}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td key={1}>
+                {transactions.reduce((accum, t) => accum + Number(t.amount), 0)}
+              </td>
+              <td key={2}></td>
+              <td key={3}></td>
+            </tr>
+          </tfoot>
+        </table>
+      )}
+    </>
   );
 }
 
